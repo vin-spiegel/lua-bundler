b75f47df99dcea28267583f06ac012bc2fac77da
Spiegel <turbogara1@gmail.com>
2022-09-13 04:19:03 +0900

    로거 추가

---
+++ lua-bundler/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Logger
+{
+    public static void Warn(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Warn - {msg}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public static void Success(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Success - {msg}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    public static void Error(string msg)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Error - {msg}");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
